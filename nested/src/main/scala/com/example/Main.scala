15cc0c943c3f60969f00ecb96ab3590e782a415e
lloydmeta <lloydmeta@gmail.com>
2017-11-30 21:16:53 +0900

    init

---
+++ nested/src/main/scala/com/example/Main.scala
@@ -0,0 +1,25 @@
+package com.example
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!!!")
+  }
+
+  /**
+   * This is how to write a doctest
+   *
+   * # Example
+   *
+   * {{{
+   * scala> Main.not(true)
+   * res0: Boolean = false
+   *
+   * // Multiline
+   * scala> Main.not(
+   *      |  false
+   *      | )
+   * res1: Boolean = true
+   * }}}
+   */
+  def not(b: Boolean): Boolean = !b
+}
